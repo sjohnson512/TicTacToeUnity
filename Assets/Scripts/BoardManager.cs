dcca3385730296e74b94cea7088a9f737c711bc5
Steve Johnson <steve.johnson512@gmail.com>
2019-07-28 11:41:32 -0500

    Add comments

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -26,11 +26,16 @@ namespace TicTacToe
         private int playerId1;
         private int playerId2;
 
-        private int emptyCellValue = 0;
-        private int player1CellValue = 1;
-        private int player2CellValue = 10;
+        // Constant values used to populate cellContents
+        // Values of 1 and 10 for players simplify the logic to check for wins later
+        private readonly int emptyCellValue = 0;
+        private readonly int player1CellValue = 1;
+        private readonly int player2CellValue = 10;
 
+        // List used to track which marker is in each cell
         private List<int> cellContents;
+
+        // Lists of which cells are members of each row
         private List<List<int>> rowMembers = new List<List<int>>
         {
             new List<int> {0, 1, 2},
@@ -43,11 +48,20 @@ namespace TicTacToe
             new List<int> {2, 4, 6}
         };
 
+        /// <summary>
+        /// The constructor sets up the board contents
+        /// </summary>
+        /// <param name="playerIdNoneIn">PlayerId set by GameManager</param>
+        /// <param name="playerId1In">PlayerId set by GameManager</param>
+        /// <param name="playerId2In">PlayerId set by GameManager</param>
         public BoardManager(int playerIdNoneIn, int playerId1In, int playerId2In)
         {
+            // Set player IDs
             playerIdNone = playerIdNoneIn;
             playerId1 = playerId1In;
             playerId2 = playerId2In;
+
+            // Create a new cell contents list and initialize it to empty cells
             cellContents = new List<int>();
             for (int i = 0; i < 9; i++)
             {
@@ -55,39 +69,53 @@ namespace TicTacToe
             }
         }
 
+        /// <summary>
+        /// Check to see if the selected cell is empty
+        /// </summary>
+        /// <param name="cellSelected">The cell index to check</param>
+        /// <returns>true if the cellSelected is empty</returns>
         public bool IsLegalMove(int cellSelected)
         {
             return cellContents[cellSelected] == playerIdNone;
         }
 
+        /// <summary>
+        /// Update the selected cell with the player's cell value
+        /// </summary>
+        /// <param name="playerId">Id of the player placing the cell</param>
+        /// <param name="cellId">Id of the selected cell</param>
         public void PlaceMarker(int playerId, int cellId)
         {
-            // For player1 store a value of 1.  For player 2 store a value of 10.  
-            // This simplifies the logic to inspect row contents later.
-
             int cellValue = emptyCellValue;
             if (playerId == playerId1)
             {
                 cellValue = player1CellValue;
             }
-            else
+            else if (playerId == playerId2)
             {
                 cellValue = player2CellValue;
-            }
+            }      
 
             cellContents[cellId] = cellValue;
         }
 
+        /// <summary>
+        /// Checks cell contents to if someone has won the game
+        /// </summary>
+        /// <returns>Returns the winning player's id and the winning rowNumber</returns>
         public (int, int) CheckForWin()
         {
+            // Iterate over all rows
             int rowNum = 0;
             List<int> rowSums = GetRowSums();
             foreach(int sum in rowSums)
             {
+                // Check if the row has 3 X's
                 if (sum == player1CellValue * 3)
                 {
                     return (playerId1, rowNum);
                 }
+                // Check if the row has 3 O's
                 else if (sum == player2CellValue * 3)
                 {
                     return (playerId2, rowNum);
@@ -98,6 +126,10 @@ namespace TicTacToe
             return (playerIdNone, -1);
         }
 
+        /// <summary>
+        /// Checks to see if all of the cells are full.
+        /// </summary>
+        /// <returns>true if all cells are full</returns>
         public bool IsTieGame()
         {
             int numFullCells = 0;
@@ -113,11 +145,14 @@ namespace TicTacToe
             return isTieGame;
         }
 
+        /// <summary>
+        /// Simple logic to let the computer pick a random empty cell
+        /// </summary>
+        /// <returns>Index for the selected cell</returns>
         public int GetComputerMove()
         {
             // Get a list of empty cells
             List<int> emptyCells = new List<int>();
-
             int cellNumber = 0;
             foreach (int cell in cellContents)
             {
@@ -128,17 +163,25 @@ namespace TicTacToe
                 cellNumber++;
             }
 
+            // Randomly select an empty cell
             int numEmptyCells = emptyCells.Count();
             int selectedIndex = Random.Range(0,numEmptyCells);
             int selectedCell = emptyCells[selectedIndex];
 
-            Debug.Log("SelectedCell: " + selectedCell.ToString());
             return selectedCell;
         }
 
+        /// <summary>
+        /// Get sum of the contents for each row.
+        /// Useful for checking for a win and for future computer logic.  
+        /// A row value of 3 indicates 3 Xs.  A  row value of 30 indicates 3 Os, etc.
+        /// </summary>
+        /// <returns>Sum of the values in the </returns>
         private List<int> GetRowSums()
         {
             List<int> rowSums = new List<int>();
+
+            // Iterate over all of the rows, computing the sum of the row's contents
             foreach (List<int> row in rowMembers)
             {
                 int sum = 0;
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,11 +24,9 @@ public class GameManager : MonoBehaviour
     static readonly private int playerId1 = 1;
     static readonly private int playerId2 = 2;
 
+    // Vars to track game state
     private int currentPlayerId = 1;
-
-    Dictionary<int, bool> playerIsComputer = new Dictionary<int, bool>();
-    
-    // Other game state info
+    Dictionary<int, bool> playerIsComputer = new Dictionary<int, bool>();   
     private bool gameIsOver = false;
 
     // Center points for each cell.  See summary of BoardManager for how cells in the board are organized.
@@ -45,6 +43,7 @@ public class GameManager : MonoBehaviour
             new Vector3( 2.0f, -2.0f, 0.0f)
         };
 
+    // Start points for line drawn through winning row
     private List<Vector3> rowStarts = new List<Vector3>
         {
             new Vector3(-2.8f,  2.0f, -1.0f),
@@ -57,6 +56,7 @@ public class GameManager : MonoBehaviour
             new Vector3( 2.8f,  2.8f, -1.0f)
         };
 
+    // End points for line drawn through winning row
     private List<Vector3> rowEnds = new List<Vector3>
         {
             new Vector3( 2.8f,  2.0f, -1.0f),
@@ -69,11 +69,14 @@ public class GameManager : MonoBehaviour
             new Vector3(-2.8f, -2.8f, -1.0f)
         };
 
-
-    // Start is called before the first frame update
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
     void Start()
     {
         // Set the player types
+        // TODO: Add an options screen to let players be set to human or computer.  For now,
+        // player1 is always human and player2 is computer.
         playerIsComputer.Add(playerId1, false);
         playerIsComputer.Add(playerId2, true);
 
@@ -90,14 +93,22 @@ public class GameManager : MonoBehaviour
         ResetGame();
     }
 
+    /// <summary>
+    /// Reset does the following:
+    /// 1. Set the current player to player1
+    /// 2. Clear the end of game text and line
+    /// 3. Reset the board manager and clear markers from the board
+    /// </summary>
     void ResetGame()
     {
         currentPlayerId = playerId1;
 
+        // Clear the end of game text and line
         endGameText.text = "";
         lineRenderer.positionCount = 0;
         lineRenderer.positionCount = 2;
 
+        // Reset the boardManager and clear all the markers from the board
         boardManager = new BoardManager(playerIdNone, playerId1, playerId2);
         if (markers.Count > 0)
         { 
@@ -108,32 +119,39 @@ public class GameManager : MonoBehaviour
         }
 
         gameIsOver = false;
-
-        return;
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Do the following once per frame:
+    /// 1. If it is the computer's turn, let it select a cell
+    /// 2. If it is the player's turn, wait for the user to click a cell
+    /// </summary>
     void Update()
     {
-
+        // Computer player's turn
         if (!gameIsOver && playerIsComputer[currentPlayerId])
         {
             int computerSelectedCell = boardManager.GetComputerMove();
-            Thread.Sleep(600);
+            Thread.Sleep(600); // TODO: do this with a coroutine instead
             HandleCellSelected(computerSelectedCell);
+            return;
         }
 
+        // Human player's turn
         if (Input.GetMouseButtonDown(0))
         {
+            // If the game is over, let the player click once to reset the game
             if (gameIsOver)
             {
                 ResetGame();
                 return;
             }
 
+            // Get the cell the player clicked on
             Vector3 clickPosition = cam.ScreenToWorldPoint(Input.mousePosition);
             int closestCell = GetClosestCell(clickPosition);
 
+            // If the player clicked on an empty cell, update the board
             if (boardManager.IsLegalMove(closestCell))
             {
                 HandleCellSelected(closestCell);
@@ -141,8 +159,13 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Given a selected cell, updates the board and checks if the game is over.
+    /// </summary>
+    /// <param name="selectedCell">Index for the selected cell.</param>
     void HandleCellSelected(int selectedCell)
     {
+        // Draw the player's marker on the board and update the boardManager
         if (currentPlayerId == playerId1)
         {
             Object newMarker = Instantiate(xMarker, cellCenters[selectedCell], Quaternion.identity);
@@ -158,19 +181,23 @@ public class GameManager : MonoBehaviour
             currentPlayerId = playerId1;
         }
 
+        // Check to see if the game is over
         var (winningPlayerId, rowId) = boardManager.CheckForWin();
-
         if (winningPlayerId == playerId1)
         {
+            // Draw a line through the winning row
             lineRenderer.SetPosition(0, rowStarts[rowId]);
             lineRenderer.SetPosition(1, rowEnds[rowId]);
+
             endGameText.text = "Player1 Wins!";
             gameIsOver = true;
         }
         else if (winningPlayerId == playerId2)
         {
+            // Draw a line through the winning row
             lineRenderer.SetPosition(0, rowStarts[rowId]);
             lineRenderer.SetPosition(1, rowEnds[rowId]);
+
             endGameText.text = "Player2 Wins!";
             gameIsOver = true;
         }
